68fc9662f537486203981f3d89863d9c1614b1a9
FORGEJackson <josh.forge.jackson@gmail.com>
2021-01-27 22:41:45 -0600

    Basic Menu implementation and converted Items dictionary to be a static member of Assign1

--- CSCI473Assign1/Assign1.cs
+++ CSCI473Assign1/Assign1.cs
@@ -16,10 +16,24 @@ namespace CSCI473Assign1
 
     class Assign1
     {
+        static Dictionary<uint, Item> Items = new Dictionary<uint, Item>();
+        static Dictionary<uint, Player> Players = new Dictionary<uint, Player>();
+        static Dictionary<uint, string> Guilds = new Dictionary<uint, string>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the World of ConflictCraft: Testing Environment!\n");
+
+            Setup();
+
+            Item test = Assign1.Items[0];
+        }
+
+        static void Setup()
+        {
+            //Load in all Items
             string curLine;
-            Dictionary<uint, Item> Items = new Dictionary<uint, Item>();
+            
 
             Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CSCI473Assign1.equipment.txt");
             StreamReader inFile = new StreamReader(stream);
@@ -39,12 +53,32 @@ namespace CSCI473Assign1
                 Items.Add(id, new Item(id, values[1], (ItemType)type, ilvl, primary, stamina, requirement, values[7]));
                 curLine = inFile.ReadLine();
             }
+        }
 
-            foreach (KeyValuePair<uint, Item>cur in Items)
+        static bool Menu()
+        {
+            int input = 0;
+
+            Console.WriteLine("Welcome to the World of ConflictCraft: Testing Environment. Please select an option from the list below:\n\t" +
+                "1.) Print All Players\n\t" +
+                "2.) Print All Guilds\n\t" +
+                "3.) List All Gear\n\t" +
+                "4.) Print Gear List for Player\n\t" +
+                "5.) Leave Guild\n\t" +
+                "6.) Join Guild\n\t" +
+                "7.) Equip Gear\n\t" +
+                "8.) Unequip Gear\n\t" +
+                "9.) Award Experience\n\t" +
+                "10.) Quit\n");
+
+            while ((input < 1 || input > 10) && input != 'T') 
             {
-                System.Console.WriteLine(cur.Value);
+                input = Console.Read();
+                if ((input < 1 || input > 10) && input != 'T')
+                    Console.WriteLine("Please enter the digit corresponding with your choice");
             }
 
+            return true;
         }
     }
 }
