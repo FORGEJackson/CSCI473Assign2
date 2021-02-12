8cd8977feadbdc3049c3009d5f2c22e30466d0b0
FORGEJackson <josh.forge.jackson@gmail.com>
2021-02-11 21:01:10 -0600

    Added Documentation

--- CSCI473Assign2/Guild.cs
+++ CSCI473Assign2/Guild.cs
@@ -13,6 +13,10 @@ namespace CSCI473Assign2
         private string name;
         private Servers location;
 
+        /*
+         * Guild - Default Constructor
+         * Sets everything to 0, "", or null
+        */
         public Guild()
         {
             this.id = 0;
@@ -21,6 +25,10 @@ namespace CSCI473Assign2
             this.location = 0;
         }
 
+        /*
+         * Guild - Constructor override
+         * This constructor is designed to create guilds from guilds.txt
+        */
         public Guild(uint id, string nameServer)
         {
             this.id = id;
@@ -32,6 +40,10 @@ namespace CSCI473Assign2
             this.location = server;
         }
 
+        /*
+         * Guild - Constructor override
+         * This constructor allows manual input of all values
+        */
         public Guild(uint id, string name, Servers server, GuildType type)
         {
             this.id = id;
@@ -40,11 +52,14 @@ namespace CSCI473Assign2
             this.type = type;
 
         }
+
+        //Getters and setters
         public uint Id { get => id; }
         public string Name { get => name; }
         public Servers Location { get => location; }
         public GuildType Type { get => type; set => type = value; }
 
+        //Override for ToString - returns [GUILDNAME] [SERVER] with padding
         public override string ToString()
         {
             return String.Format("{0} [{1}]\r\n",this.Name.PadRight(24, ' '), this.Location.ToString());
